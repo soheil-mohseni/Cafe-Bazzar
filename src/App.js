0c315c16c294ed21f54098b5b286f47e09abf9bf
soheil8170 <mohsenisoheil61@gmail.com>
2022-10-18 09:25:35 +0330

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import AddWork from "./components/aa";
+// import BazaarMenu from "./components/bazaarmenu";
+//import Bazaarbutton from "./components/ButtonAppBar";
+// import Bazaarbutton from "./components/ButtonAppBar";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <AddWork/>
+    </div>
+  );
+}
+
+export default App;
